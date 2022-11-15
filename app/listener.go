4808c99b47ef26377bfb4049326e09fb7e6f71c9
Dwi Hujianto <dwi.hujianto@gmail.com>
2022-11-15 08:17:53 +0700

    init

---
+++ app/listener.go
@@ -0,0 +1,32 @@
+package app
+
+import (
+	"fmt"
+	"net"
+
+	"context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"pingme/rpc"
+)
+
+type svc struct {
+	rpc.UnimplementedPingMeServer
+}
+
+func (svc) PingMe(c context.Context, p *rpc.PingArg) (*rpc.PingArg, error) {
+	return p, nil
+}
+
+func StartGrpc(addr string, port int) error {
+	addr = fmt.Sprintf(":%d", port)
+	listen, err := net.Listen("tcp4", addr)
+	if err != nil {
+		return err
+	}
+	var s = grpc.NewServer()
+	rpcServer := &svc{}
+	rpc.RegisterPingMeServer(s, rpcServer)
+	reflection.Register(s)
+	return s.Serve(listen)
+}
---
+++ pingme.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+
+	"pingme/app"
+
+)
+
+func main() {
+	ch := make(chan error)
+
+	go func(c chan error) {
+		c <- app.StartGrpc("localhost", 12345)
+	}(ch)
+
+	log.Println("Listen")
+
+	for a := range ch {
+		log.Println(a)
+	}
+}
